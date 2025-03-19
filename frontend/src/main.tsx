d8d51a804607e9ccd12e71c610d119ab58ee0ab7
Tejaswa Jhode <jhodetejswa2004@gmail.com>
2025-03-19 10:19:22 +0530

    reload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <HashRouter>
+  <App />
+</HashRouter>
 );
+
+
